9b9f5cdc57872b0edac476be0b3fd164b29eab95
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-02-18 08:53:39 +0000

    check changes

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React  from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Movie App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
