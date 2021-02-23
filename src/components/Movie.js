149b03b83ff61b1434fc5907cdfa4322c1cabe09
Sean <53915643+sampelmedia@users.noreply.github.com>
2021-02-23 13:52:58 +0000

    change index css - color, format

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,17 @@
 import React from "react";
+import "../index.css"
 
 const img_api = "http://image.tmdb.org/t/p/w1280";
 
-const Movie = ({title, poster_path, overview, vote_average}) => <div 
+const Movie = ({title, poster_path, overview, vote_average}) => (
+<div 
   className="movie">
       <img src={img_api + poster_path} alt={title}/>
-  </div>;
-
+      <div className="movie-info">
+        <h3>{title}</h3>
+        <span>{vote_average}</span>
+      </div>
+  </div>
+);
 
 export default Movie;
